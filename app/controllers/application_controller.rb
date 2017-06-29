9193e3e9d5b618b7afd11cc4c1a37059aebbdc8a
yasushiasahi <asahi1600@gmail.com>
2017-06-29 19:23:56 +0000

    want

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < ActionController::Base
       redirect_to login_url
     end
   end
+  
+  private
+
+  def read(result)
+    code = result.code
+    name = result['itemName']
+    url = result.url
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
 end
